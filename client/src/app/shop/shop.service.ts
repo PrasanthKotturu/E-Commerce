3dad4c1655202eed0bfea7cd2b3167004403196b
Prasanth <kvlprasanth92@gmail.com>
2021-11-14 23:38:41 +0530

    Section 10 completed

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagenation';
 import { IProductType } from '../shared/models/producttype';
 import { map } from 'rxjs/operators'
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -13,25 +14,28 @@ export class ShopService {
   baseUrl = "https://localhost:5001/api/"
   constructor(private http: HttpClient) { }
 
-  getProducts(shopParams:ShopParams) {
+  getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
-    if (shopParams.brandId!==0) {
+    if (shopParams.brandId !== 0) {
       params = params.append('brandId', shopParams.brandId.toString());
     }
-    if (shopParams.typeId!==0) {
+    if (shopParams.typeId !== 0) {
       params = params.append('typeId', shopParams.typeId.toString());
     }
-    if(shopParams.search){
-      params=params.append('search',shopParams.search);
+    if (shopParams.search) {
+      params = params.append('search', shopParams.search);
     }
-      params = params.append('sort', shopParams.sort);
-      params = params.append('pageIndex', shopParams.pageNumber.toString());
-      params = params.append('pageSize', shopParams.pageSize.toString());
-    
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber.toString());
+    params = params.append('pageSize', shopParams.pageSize.toString());
+
     return this.http.get<IPagination>(this.baseUrl + 'products', { observe: 'response', params })
       .pipe(
         map(response => { return response.body })
       )
+  }
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + "products/" + id)
   }
   getProductBrands() {
     return this.http.get<IBrand[]>(this.baseUrl + "products/productbrands");
