6c6ca7b14f330775b827b98671dd6030e988ac72
Prasanth <kvlprasanth92@gmail.com>
2023-02-03 22:08:07 +0530

    commit

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,11 +5,12 @@ import { RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent, NotFoundComponent, ServerErrorComponent],
+  declarations: [NavBarComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
     RouterModule,
@@ -19,7 +20,8 @@ import { ToastrModule } from 'ngx-toastr';
     })
   ],
   exports:[
-    NavBarComponent
+    NavBarComponent,
+    SectionHeaderComponent
   ]
 })
 export class CoreModule { }
